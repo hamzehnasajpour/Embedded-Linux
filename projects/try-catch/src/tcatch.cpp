8a569f0272705c44008c35c365f937e987bdab92
h.nasajpour <h.nasajpour@pantherx.org>
2022-09-18 00:43:10 +0430

    seg fault catch

---
+++ projects/try-catch/src/tcatch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+
+using namespace std;
+void segfault_sigaction(int signal, siginfo_t *si, void *arg)
+{
+    printf("Caught segfault at address %p\n", si->si_addr);
+    exit(0);
+}
+
+int main(void)
+{
+    int *foo = NULL;
+    struct sigaction sa;
+
+    memset(&sa, 0, sizeof(struct sigaction));
+    sigemptyset(&sa.sa_mask);
+    sa.sa_sigaction = segfault_sigaction;
+    sa.sa_flags   = SA_SIGINFO;
+
+    sigaction(SIGSEGV, &sa, NULL);
+
+    /* Cause a seg fault */
+    *foo = 1;
+
+    return 0;
+}
